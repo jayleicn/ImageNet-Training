91491116e6182a43d8bff41ca6f2f04e473e4560
Jie Lei <jay-lei@foxmail.com>
2016-08-22 18:40:19 -0500

    Update CreateLMDBs.lua

--- CreateLMDBs.lua
+++ CreateLMDBs.lua
@@ -7,7 +7,7 @@ local DataProvider = require 'DataProvider'
 local config = require 'Config'
 
 -------------------------------Settings----------------------------------------------
-
+-- rescale the image without change its aspect ratio, the final ouput is a 3*H*W, min(H, W) = config.ImageMinSide
 local PreProcess = function(Img)
     local im = image.scale(Img, '^' .. config.ImageMinSide) --minimum side of ImageMinSide
 
@@ -23,15 +23,18 @@ local PreProcess = function(Img)
     return im
 end
 
-
+-- 
 local LoadImgData = function(filename)
-    local img = gm.Image(filename):toTensor('float','RGB','DHW')
+--    local img = gm.Image(filename):toTensor('float','RGB','DHW') -- Load images to tensor [filename is the file path] 
+--  Why use gm to load?  for effecency?
+    local img = image.load(filename, 3, 'float')
     if img == nil then
         print('Image is buggy')
         print(filename)
         os.exit()
     end
     img = PreProcess(img)
+    -- any need to compress the images ???
     if config.Compressed then
         return image.compressJPG(img)
     else
@@ -39,9 +42,11 @@ local LoadImgData = function(filename)
     end
 end
 
+
+-- images are all named in the format "wnid_seriesNum"
 function NameFile(filename)
-    local name = paths.basename(filename,'JPEG')
-    local substring = string.split(name,'_')
+    local name = paths.basename(filename,'JPEG')  --filename without path prefix and format suffix
+    local substring = string.split(name,'_')  -- return a table
 
     if substring[1] == 'ILSVRC2012' then -- Validation file
         local num = tonumber(substring[3])
@@ -52,15 +57,18 @@ function NameFile(filename)
 
 end
 
+
+
+
 function LMDBFromFilenames(filenamesProvider,env)
     env:open()
     local txn = env:txn()
     local cursor = txn:cursor()
     for i=1, filenamesProvider:size() do
-        local filename = filenamesProvider:getItem(i)
-        local data = {Data = LoadImgData(filename), Name = NameFile(filename)}
+        local filename = filenamesProvider:getItem(i)  --filename is the image full path
+        local data = {Data = LoadImgData(filename), Name = NameFile(filename)} --image itself and its name as one entry
 
-        cursor:put(config.Key(i),data, lmdb.C.MDB_NODUPDATA)
+        cursor:put(config.Key(i),data, lmdb.C.MDB_NODUPDATA)  --config.Key(i) generate a formated stringed_number "
         if i % 1000 == 0 then
             txn:commit()
             print(env:stat())
