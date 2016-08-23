891f2c88c54de2b3c83c5b1c4593d4e5cbd2c22c
Jie Lei <jay-lei@foxmail.com>
2016-08-22 22:07:08 -0500

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,8 +1,10 @@
 local ImageNetClasses = torch.load('./ImageNetClasses')
+-- nil the useless data entry
 for i=1001,#ImageNetClasses.ClassName do
     ImageNetClasses.ClassName[i] = nil
 end
 
+--as lmdb entry keys
 function Key(num)
     return string.format('%07d',num)
 end
@@ -15,9 +17,15 @@ return
     VALIDATION_DIR = '/home/ehoffer/Datasets/ImageNet/LMDB/validation/', --Validation LMDB location
     TRAINING_DIR = '/home/ehoffer/Datasets/ImageNet/LMDB/train/', --Training LMDB location
     ImageMinSide = 256, --Minimum side length of saved images
-    ValidationLabels = torch.load('./ValidationLabels'),
+    ValidationLabels = torch.load('./ValidationLabels'),  -- Torch Tensor, 50000*1, eg. ValidationLabels[1]=490
     ImageNetClasses = ImageNetClasses,
     Normalization = {'simple', 118.380948, 61.896913}, --Default normalization -global mean, std
     Compressed = true,
     Key = Key
 }
+
+-- ImageNetClasses -- Lua Table
+-- ClassNum2Wnid    {1:n01972223, 2:n02234243, ...}
+-- Wnid2ClassNum    nil, 
+-- Description
+-- ClassName
