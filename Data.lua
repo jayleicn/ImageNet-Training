9d28277975e67a290c453efc8144280eb0c33783
Jie Lei <jay-lei@foxmail.com>
2016-08-23 12:08:33 -0500

    Update Data.lua

--- Data.lua
+++ Data.lua
@@ -7,8 +7,25 @@ local config = require 'Config'
 
 
 function ExtractFromLMDBTrain(data)
+    
+    -- two items Name, Data in one LMDB entry
+    local wnid = string.split(data.Name,'_')[1]
+    local class = config.ImageNetClasses.Wnid2ClassNum[wnid]  -- from here we can also get coarse labels
+    -- local class_coarse, class_fine
+    local img = data.Data
+    if config.Compressed then
+        img = image.decompressJPG(img,3,'byte')
+    end
+
+    if math.min(img:size(2), img:size(3)) ~= config.ImageMinSide then
+        img = image.scale(img, '^' .. config.ImageMinSide)
+    end
+    
+    
+    -- Data augementation
     require 'image'
-    --
+    
+    -- narrow(dim,index,size) an view of the storage in dimmension [dim], from index to index+size-1
     local reSample = function(sampledImg)
         local sizeImg = sampledImg:size()
         local szx = torch.random(math.ceil(sizeImg[3]/4)) -- width
@@ -27,29 +44,19 @@ function ExtractFromLMDBTrain(data)
         return applyRot
     end
     
-    -- two items Name, Data in one LMDB entry
-    local wnid = string.split(data.Name,'_')[1]
-    local class = config.ImageNetClasses.Wnid2ClassNum[wnid]  -- from here we can also get coarse labels
-    -- local class_coarse, class_fine
-    local img = data.Data
-    if config.Compressed then
-        img = image.decompressJPG(img,3,'byte')
-    end
-
-    if math.min(img:size(2), img:size(3)) ~= config.ImageMinSide then
-        img = image.scale(img, '^' .. config.ImageMinSide)
-    end
-    -- Data augementation
     if config.Augment == 3 then
         img = rotate(0.1)(img)
         img = reSample(img)
     elseif config.Augment == 2 then
         img = reSample(img)
     end
+    
+    -- crop img randomly to the model.InputSize OR default config.InputSize {3,224,224}
     local startX = math.random(img:size(3)-config.InputSize[3]+1)
     local startY = math.random(img:size(2)-config.InputSize[2]+1)
-
     img = img:narrow(3,startX,config.InputSize[3]):narrow(2,startY,config.InputSize[2])
+
+    -- mirror (horizontally)
     local hflip = torch.random(2)==1
     if hflip then
         img = image.hflip(img)
@@ -70,13 +77,16 @@ function ExtractFromLMDBTest(data)
     if (math.min(img:size(2), img:size(3)) ~= config.ImageMinSide) then
         img = image.scale(img, '^' .. config.ImageMinSide)
     end
-
-    local startX = math.ceil((img:size(3)-config.InputSize[3]+1)/2)
+    
+    
+    local startX = math.ceil((img:size(3)-config.InputSize[3]+1)/2)  -- Why /2
     local startY = math.ceil((img:size(2)-config.InputSize[2]+1)/2)
     img = img:narrow(3,startX,config.InputSize[3]):narrow(2,startY,config.InputSize[2])
+    
     return img, class
 end
 
+-- simply a formation of the numbers; eg. tensor --> tb1, 234 --> "0000234"     
 function Keys(tensor)
     local tbl = {}
     for i=1,tensor:size(1) do
@@ -85,16 +95,17 @@ function Keys(tensor)
     return tbl
 end
 
+
 function EstimateMeanStd(DB, typeVal, numEst)
-    local typeVal = typeVal or 'simple'
+    local typeVal = typeVal or 'simple'  -- normalize type, all-images/channel-wise/
     local numEst = numEst or 10000
     local x = torch.FloatTensor(numEst ,unpack(config.InputSize))
-    local randKeys = Keys(torch.randperm(DB:size()):narrow(1,1,numEst))
-    DB:CacheRand(randKeys, x)
-    local dp = DataProvider.Container{
+    local randKeys = Keys(torch.randperm(DB:size()):narrow(1,1,numEst))  --randKeys will be a formatted randperm
+    DB:CacheRand(randKeys, x)  -- discard the label arg, the data will be stored in local variable "x"
+    local dp = DataProvider.Container{  
         Source = {x, nil}
     }
-    return {typeVal, dp:normalize(typeVal)}
+    return {typeVal, dp:normalize(typeVal)}  -- normalize type, mean, std   -- x has been normalized
 end
 
 local TrainDB = DataProvider.LMDBProvider{
